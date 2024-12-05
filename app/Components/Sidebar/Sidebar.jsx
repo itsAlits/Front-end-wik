07068b83daa60a8123b94c6269783ec6d280fbaf
itsAlitss <alitwiradhanta094@student.unud.ac.id>
2024-12-05 20:30:36 +0800

    updt

--- app/Components/Sidebar/Sidebar.jsx
+++ app/Components/Sidebar/Sidebar.jsx
@@ -129,11 +129,11 @@ export default function Sidebar() {
         </Link>
 
         <div onClick={handleLogout} className={`${getLinkClass("")} cursor-pointer`}>
-          <div className="flex items-center gap-2">
+          <div className="flex items-center gap-2 alits">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
               <path strokeLinecap="round" strokeLinejoin="round" d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3" />
             </svg>
-            <h1 className="text-sm">Logout</h1>
+            <h1 className="text-sm">Log Out</h1>
           </div>
         </div>
       </div>
